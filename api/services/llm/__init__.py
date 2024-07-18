7d684e4f9ee11ef1ac18d68a6f47feac8b38a96c
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2024-07-18 19:42:38 +0100

    Fix: path

--- api/services/llm/__init__.py
+++ api/services/llm/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 blueprint = Blueprint(
     'llm',
     __name__,
-    url_prefix='/llm',
+    url_prefix='/core/llm',
     template_folder='templates',
     static_folder='static'
 )
