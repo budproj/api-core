8dceb82733157780687f6d7354ce1d505d02b43f
Rodrigo Antas <rodr.antas@gmail.com>
2024-07-25 10:42:47 -0300

    feat(summary.py): adding user,check-in, comment, check-mark to the summarization

---
+++ api/models/db/key_result_comment.py
@@ -0,0 +1,29 @@
+import uuid
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship
+
+from .types.key_result_format_enum import KeyResultFormatEnum
+from .types.key_result_mode_enum import KeyResultModeEnum
+from .types.key_result_type_enum import KeyResultTypeEnum
+
+from .objective import Objective
+from .user import User
+
+from .base import Base
+
+
+class KeyResultComment(Base):
+    __tablename__ = 'key_result_comment'
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, nullable=False)
+    text: Mapped[str] = mapped_column(nullable=False)
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    key_result_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey('key_result.id'), nullable=False)
+
+    key_result = relationship(
+        'KeyResult', back_populates='key_result_comments')
+    user = relationship(
+        'User', back_populates='key_result_comments')
