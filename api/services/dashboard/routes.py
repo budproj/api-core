eecaf0302a266010da30bc3e3713083fb9fde5ef
Rodrigo Antas <rodr.antas@gmail.com>
2024-08-18 17:58:06 -0300

    removing prints

--- api/services/dashboard/routes.py
+++ api/services/dashboard/routes.py
@@ -6,14 +6,10 @@ from flask import render_template, request
 @blueprint.route('/team-ranking/<team_id>')
 def team_ranking(team_id):
     cycle_ids = request.args.getlist('cycle_ids')
-    print(cycle_ids)
-    print("aaaaaaaaa")
     return render_template('team-ranking.html', opts=TeamRanking(team_id, cycle_ids))
 
 
 @blueprint.route('/team-ranking/<team_id>/children')
 def team_ranking_children(team_id):
     cycle_ids = request.args.getlist('cycle_ids')
-    print(cycle_ids)
-    print("bbbbbbbb")
     return render_template('team-ranking-content.html', opts=TeamRanking(team_id, cycle_ids, load_cycles=False))
